dcb0d4f27c51b2be2b8775012420c8ea26fc6de7
José Lucas Bettiol da Costa <85143931+LucasBDC@users.noreply.github.com>
2023-05-04 13:06:49 -0400

    Composer

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  require 'requires\head.php';
+?>
+<body class="tinder-background">
+      <?php require 'requires/header-nav.php' ?>
+      <main class="landing-page">
+        <header><h2 class="home-slogan">Conheça seu parceiro de campus</h2></header>
+        <section> <a href="cadastro.php" class="button-start">Criar uma conta</a></section>
+      </main>
+
+
+  <script src="script.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php require 'requires/head.php'; ?>
+<body class="tinder-background">
+    <?php require 'requires/header-nav.php' ?>
+    <div class="container">
+        
+    </div>
+
+</body>
+</html>
