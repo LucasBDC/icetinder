67fbd4895db99c8155cab5cb3fe2ea80b943ee3a
José Lucas Bettiol da Costa <85143931+LucasBDC@users.noreply.github.com>
2023-05-07 12:45:45 -0400

    update cadastro

--- cadastro.php
+++ cadastro.php
@@ -6,41 +6,60 @@
 <div class="wrapper">
   <div class="container-cadastro">
     <form action="">
-    <div class="left-cad">
-        <input type="file" class="float-left-cadastro" name="photo" id="" accept="Image/*"> 
-        <input type="file" class="float-left-cadastro" name="photo" id="" accept="Image/*"> 
-        <input type="file" class="float-left-cadastro" name="photo" id="" accept="Image/*"> 
+      <div class="left-cad">
+        <div class="photos">
+          <h3>Suas Fotos:</h3>
+          <div class="photo-1">
+            <img src="" id="photo1" alt="">
+            <input type="file" class="float-left-cadastro" name="photo" id="photo-up1" accept="Image/*">
+          </div> 
+          <div class="photo-2">
+            <img src="" id="photo2" alt="">
+            <input type="file" class="float-left-cadastro" name="photo" id="photo-up2" accept="Image/*"> 
+          </div>
+          <div class="photo-3">
+            <img src="" id="photo3" alt="">
+            <input type="file" class="float-left-cadastro" name="photo" id="photo-up3" accept="Image/*"> 
+          </div>
+        </div>
         <label for="localizacao">Localização:</label>
-        <input type="text" name="localizacao" id="">
-        <label for="sexo">Sexo:</label>
-        <select name="sexo" id="">
-          <option value="homem">Homem</option>
-          <option value="mulher">Mulher</option>
-        </select>
-        <label for="preferencia">Preferência</label>
-        <select name="preferencia" id="">
-          <option value="homem">Homem</option>
-          <option value="mulher">Mulher</option>
-          <option value="todos">Todos</option>
-        </select>
-        <label for="idade">Idade:</label>
-        <input type="number" name="idade" id="">
+        <input type="text" name="localizacao" id="localizacao">
+        <div class="three-grid">
+          <div class="grid-1">
+            <label for="sexo">Sexo:</label><br>
+            <select name="sexo" id="sex">
+              <option value="homem">Homem</option>
+              <option value="mulher">Mulher</option>
+            </select>
+          </div>
+          <div class="grid-2">
+            <label for="preferencia">Preferência:</label><br>
+            <select name="preferencia" id="pref">
+              <option value="homem">Homem</option>
+              <option value="mulher">Mulher</option>
+              <option value="todos">Todos</option>
+            </select>
+          </div>
+          <div class="grid-3">
+            <label for="idade">Idade:</label><br>
+            <input type="number" name="idade" id="idade">
+          </div>
+        </div>
       </div>
       <div class="right-cad">
         <label for="nome">Nome:</label>
-        <input type="text" name="nome">
+        <input type="text" name="nome" id="nome">
         <label for="email">Email:</label>
-        <input type="email" name="email">
+        <input type="email" name="email" id="email">
         <label for="senha">Senha:</label>
-        <input type="password" name="senha">
+        <input type="password" name="senha" id="senha">
         <label for="description">Sobre você</label>
-        <input type="text" name="description" id="">
+        <textarea name="description" class="sobre-voce" id="description" cols="30" rows="10"></textarea>
       </div>
       <div class="buttons-form clear">
-       <button type="submit" class="button-enter">Cadastrar</button>
-       </div>
+        <button type="submit" class="button-enter" id="cadastrar-button">Cadastrar</button>
+      </div>
     </form>
   </div>
 </div>
-</body>
-</html>
+<?php require 'requires/footer.php' ?>
