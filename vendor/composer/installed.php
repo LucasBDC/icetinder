9949d004a53d8cbb10b8eab4b8e28b01d75945a7
José Lucas Bettiol da Costa <85143931+LucasBDC@users.noreply.github.com>
2023-06-26 08:55:44 -0400

    feat: composer and vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jluca/icetinder',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '35eea06e21243345c3e3799348ca7dd6aadd7940',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jluca/icetinder' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '35eea06e21243345c3e3799348ca7dd6aadd7940',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
